b45e27e6d2670bc4a2f9c793ad39a5318d3301d7
lucaoskaique <lucas.kaique@tech4h.com.br>
2023-02-28 12:48:59 -0300

    finished first class

--- classes/class01/pages/video-player/src/controller.js
+++ classes/class01/pages/video-player/src/controller.js
@@ -1,14 +1,69 @@
-export default class Controller{
-  constructor({}){
 
+export default class Controller{
+  #view;
+  #service;
+  #camera;
+  #worker;
+  #blinkCounter = 0;
+  constructor({view, service, worker, camera}){
+    this.#view = view;
+    this.#camera = camera;
+    this.#worker = this.#configureWorker(worker);
+
+    this.#view.configureOnBtnClick(this.onBtnStart.bind(this));
   }
 
   static async initialize(deps){
     const controller = new Controller(deps);
+    controller.log("not yet detecting eye blink");
     return controller.init();
+  }
+
+  #configureWorker(worker){
+    let ready = false;
+    worker.onmessage = ({ data }) => {
+      console.log("Worker received message", data);
+      if(data === "READY"){
+        console.log("Worker is ready");
+        this.#view.enableButton()
+        ready = true;
+        return;
+      }
+      const blinked = data.blinked;
+      this.#blinkCounter += blinked;
+      this.#view.touglePlayVideo()
+      console.log("Blinked", blinked);
+    }
+    return {
+      send (data){
+        if(!ready) return;
+        worker.postMessage(data);
+      }
+    };
   }
 
   async init(){
     console.log("Controller initialized");
+  }
+
+  loop(){
+    const video = this.#camera.video;
+    const img = this.#view.getVideoFrame(video);
+
+    this.#worker.send(img);
+    this.log(`Detecting eye blink: ${this.#blinkCounter}`);
+
+    setTimeout(() => this.loop, 100);
+  }
+
+  log(text){
+    const times = `  - blinked times: ${this.#blinkCounter}`
+    this.#view.log(`status: ${times}`.concat(this.#blinkCounter ? times : ""));
+  }
+
+  onBtnStart(){
+    this.log("initializing detecting eye blink");
+    this.#blinkCounter = 0;
+    this.loop();
   }
 }
--- classes/class01/pages/video-player/src/view.js
+++ classes/class01/pages/video-player/src/view.js
@@ -1,5 +1,38 @@
 export default class View {
-  constructor({  }) {
-    
+  #btnInit = document.querySelector("#init");
+  #statusElement = document.querySelector("#status");
+  #videoFrameCanvas = document.createElement("canvas");
+  #canvasContext = this.#videoFrameCanvas.getContext("2d", {
+    willReadFrequently: true,
+  });
+  #videoElement = document.querySelector("#video");
+
+  enableButton() {
+    this.#btnInit.disabled = false;
+  }
+
+  touglePlayVideo() {
+    if (this.#videoElement.paused) {
+      this.#videoElement.play();
+      return
+    }
+    this.#videoElement.pause();
+  }
+
+  getVideoFrame(video) {
+    const canvas = this.#videoFrameCanvas;
+    const [width, height] = [video.videoWidth, video.videoHeight];
+    canvas.width = width;
+    canvas.height = height;
+    this.#canvasContext.drawImage(video, 0, 0, width, height);
+    return this.#canvasContext.getImageData(0, 0, width, height);
+  }
+
+  configureOnBtnClick(callback) {
+    this.#btnInit.addEventListener("click", callback);
+  }
+
+  log(text) {
+    this.#statusElement.innerHTML = text;
   }
 }
